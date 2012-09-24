06daf48433c845dca4bbdf77ff5d8ec6e6758a03
Josh Larson <jplhomer@gmail.com>
2012-09-24 13:51:35 -0500

    Changed to Paul Irish object notation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,53 @@
-$(document).ready(function() {
+// When you change APPName, be sure to update it in mylibs/util.js
+// @see http://paulirish.com/2009/markup-based-unobtrusive-comprehensive-dom-ready-execution/
+var APPNAME = {
+  
+  methodName: function() {
+    
+  },
+  
+  // Initializers
+  common: {
+    init: function() { 
+      
+    },
+    finalize: function() {
+      
+    }
+  },
+  
+  bodyId_or_className: {
+    init: function() { 
 
-// Place any scripts to be run on document.ready here.    
+    },
+    finalize: function() { 
+      
+    }
+  }
+};
 
-}
+UTIL = {
+  fire: function( func,funcname, args ) {
+    var namespace = APPNAME;  // indicate your obj literal namespace here
+ 
+    funcname = ( funcname === undefined ) ? 'init' : funcname;
+    if ( func !== '' && namespace[ func ] && typeof namespace[ func ][ funcname ] == 'function' ) {
+      namespace[ func ][ funcname ]( args );
+    }
+  },
+  loadEvents: function() {
+    var bodyId = document.body.id;
+ 
+    // hit up common first.
+    UTIL.fire( 'common' );
+ 
+    // do all the classes too.
+    $.each( document.body.className.split( /\s+/ ), function( i, classnm ) {
+      UTIL.fire( classnm );
+      UTIL.fire( classnm, bodyId );
+    });
+    UTIL.fire( 'common', 'finalize' );
+  }
+};
+
+$(document).ready(UTIL.loadEvents);
